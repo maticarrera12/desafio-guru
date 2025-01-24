37a73b8e904dec60580a629aa321ab64daa21a11
MatiCarrera <mcarreradev12@gmail.com>
2025-01-23 21:08:14 -0300

    tercer commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/desafio-guru/', // Asegúrate de que coincida con el nombre de tu repositorio
 })
